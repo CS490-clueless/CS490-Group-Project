17dcaab83e97de7a95966830991087b36d65775b
chloeyyao <yao278@purdue.edu>
2023-03-08 18:44:58 -0500

    login and sign up screens

--- dapperling/dapperling/AppDelegate.swift
+++ dapperling/dapperling/AppDelegate.swift
@@ -6,16 +6,54 @@
 //
 
 import UIKit
+import ParseSwift
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
-
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+
+        
+        // TODO: Pt 1 - Initialize Parse SDK
+        ParseSwift.initialize(applicationId: "VrmslyE8hYXfRM3HuiRK8YO97qPIqx2BzqzFqJ0m",
+                              clientKey: "SK9PaCdfsuxcGpjp8cU2AtiFGqMUZlCXf2Q626Dz",
+                              serverURL: URL(string: "https://parseapi.back4app.com")!)
+        
+//        // Instantiate the test parse object
+//        var score = GameScore()
+//        score.playerName = "Kingsley"
+//        score.points = 13
+//
+//        // Save to your server asynchronously (preferred way) - Performs work on background queue and returns to specified callbackQueue.
+//        // If no callbackQueue is specified it returns to main queue.
+//        score.save { result in
+//            switch result {
+//            case .success(let savedScore):
+//                print("✅ Parse Object SAVED!: Player: \(String(describing: savedScore.playerName)), Score: \(String(describing: savedScore.points))")
+//            case .failure(let error):
+//                assertionFailure("Error saving: \(error)")
+//            }
+//        }
+    
+
+        // https://github.com/parse-community/Parse-Swift/blob/main/ParseSwift.playground/Sources/Common.swift
+
+
+        // TODO: Pt 1: - Instantiate and save a test parse object to your server
+        // https://github.com/parse-community/Parse-Swift/blob/3d4bb13acd7496a49b259e541928ad493219d363/ParseSwift.playground/Pages/1%20-%20Your%20first%20Object.xcplaygroundpage/Contents.swift#L121
+
+
         return true
     }
+
+
+
+
+//    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+//        // Override point for customization after application launch.
+//        return true
+//    }
 
     // MARK: UISceneSession Lifecycle
 
