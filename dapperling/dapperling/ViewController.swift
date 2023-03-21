de31c24c3bb746cda0babbd6abcd0b05b6f2760d
cadchan04 <chan377@purdue.edu>
2023-03-20 20:25:27 -0400

    Added main view controller

--- dapperling/dapperling/ViewController.swift
+++ dapperling/dapperling/ViewController.swift
@@ -6,9 +6,13 @@
 //
 
 import UIKit
+import ParseSwift
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var Accessory: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
